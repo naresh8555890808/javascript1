8fe9c61d85e9abd9fee46d1cda61df34adf8fb66
naresh8555890808 <141204145+naresh8555890808@users.noreply.github.com>
2023-08-23 18:55:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//data types in java script
+//string data type
+var str=("hello world")
+console.log(str)
+
+//integer data type
+
+var int=23
+console.log(int)
+
+//float values in datatypes
+
+var float=2.8
+console.log(float)
+
+//bbolean values in data types 
+//boolean meand true or false
+
+var bool=true
+console.log(bool)
+
+//decleared variables in java script
+
+var a=29
+var b=39
+var c=a+b
+console.log(c)
+
+
+//crea of circle
+
+const radius=58
+const pi=3.14
+const areaofcircle=(pi*radius*radius)
+console.log(areaofcircle)
+
+//circumference of circle
+
+const r=78
+const pie=3.14
+const circumferenceofcircle=(2*pie*r)
+console.log(circumferenceofcircle)
+
+//radius of circle
+
+const rad=34
+const piee=3.14
+const radiusofcircle=(piee*rad*rad)
+console.log(radiusofcircle)
